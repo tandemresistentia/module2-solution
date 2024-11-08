068247f5e02ca5ccfa7134147a439dbb06e17325
tandemresistentia <deathghoul41@gmail.com>
2024-11-08 15:02:56 +0100

    uuu

---
+++ js/controllers/tobuy.controller.js
@@ -0,0 +1,18 @@
+﻿(function() {
+    'use strict';
+    
+    angular
+        .module('ShoppingListCheckOff')
+        .controller('ToBuyController', ToBuyController);
+        
+    ToBuyController.$inject = ['ShoppingListCheckOffService'];
+    function ToBuyController(ShoppingListCheckOffService) {
+        var toBuy = this;
+        
+        toBuy.items = ShoppingListCheckOffService.getToBuyItems();
+        
+        toBuy.buyItem = function(itemIndex) {
+            ShoppingListCheckOffService.buyItem(itemIndex);
+        };
+    }
+})();
---
+++ js/services/shopping-list.service.js
@@ -0,0 +1,37 @@
+﻿(function() {
+    'use strict';
+    
+    angular
+        .module('ShoppingListCheckOff')
+        .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+    
+    function ShoppingListCheckOffService() {
+        var service = this;
+        
+        // List of items to buy
+        var toBuyItems = [
+            { name: "cookies", quantity: 10 },
+            { name: "apples", quantity: 5 },
+            { name: "milk cartons", quantity: 2 },
+            { name: "bread loaves", quantity: 3 },
+            { name: "chocolate bars", quantity: 7 }
+        ];
+        
+        // List of bought items
+        var boughtItems = [];
+        
+        service.getToBuyItems = function() {
+            return toBuyItems;
+        };
+        
+        service.getBoughtItems = function() {
+            return boughtItems;
+        };
+        
+        service.buyItem = function(itemIndex) {
+            var item = toBuyItems[itemIndex];
+            toBuyItems.splice(itemIndex, 1);
+            boughtItems.push(item);
+        };
+    }
+})();
